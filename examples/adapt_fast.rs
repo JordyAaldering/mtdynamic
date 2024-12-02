83d0ea8401351886621beb5b6f9c6c5110f4fe47
Jordy Aaldering <aalderingjordy@gmail.com>
2024-12-02 14:17:48 +0100

    shorten adapt_fast

--- examples/adapt_fast.rs
+++ examples/adapt_fast.rs
@@ -15,18 +15,12 @@ fn main() {
         (16, true)
     };
 
-    const CYCLES: [(usize, bool); 10] = [
-        // Without pinning
+    const CYCLES: [(usize, bool); 6] = [
         ( 500, false),
-        ( 750, false),
         (1000, false),
-        (1250, false),
         (1500, false),
-        // With pinning
         ( 500, true),
-        ( 750, true),
         (1000, true),
-        (1250, true),
         (1500, true),
     ];
 
@@ -45,17 +39,19 @@ fn main() {
         let y = black_box(Matrix::random(size, size));
 
         for _ in 0..200 {
+            let num_threads = mtd.num_threads() as usize;
+            let pool = threadpool(num_threads, pin_threads);
+
             rapl.reset();
             let instant = Instant::now();
 
-            let num_threads = mtd.num_threads() as usize;
-            let pool = threadpool(num_threads, pin_threads);
             let _ = black_box(mtd.install(|| pool.install(|| x.mul(&y))));
 
             let runtime = instant.elapsed();
             let energy = rapl.elapsed();
+
             let runtime = runtime.as_secs_f32();
-            let energy: f32 = energy.values().sum();
+            let energy = energy.values().sum::<f32>();
             println!("{},{},{},{},{}", size, pin_threads, mtd.num_threads, runtime, energy);
         }
     }
